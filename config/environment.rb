1afe0a324beb4f10f36d3edeffe9f7664423d207
Berry Straver <bms@magistraver.nl>
2016-08-30 08:35:19 +0000

    MAGISTRAVER MAIL

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,23 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
+    :address => 'smtp.magistraver.nl',
+    :port => '2525',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['MAGISTRAVER_USERNAME'],
+    :password => ENV['MAGISTRAVER_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
 
 }
+
+#ActionMailer::Base.smtp_settings = {
+#    :address => 'smtp.sendgrid.net',
+#    :port => '587',
+#    :authentication => :plain,
+#    :user_name => ENV['SENDGRID_USERNAME'],
+#    :password => ENV['SENDGRID_PASSWORD'],
+#    :domain => 'heroku.com',
+#    :enable_starttls_auto => true
+
+#}
